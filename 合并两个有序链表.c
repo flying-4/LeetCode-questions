9f2fd8a941e192832aee9c1da36d0fa36080ddd2
flying-4 <468848074@qq.com>
2026-03-21 10:07:00 +0800

    合并两个有序链表.c

--- 合并两个有序链表.c
+++ 合并两个有序链表.c
@@ -25,3 +25,6 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
 	p->next=list1==NULL?list2:list1;//连接剩下未比较的节点
 	return head.next;//返回第一个有效节点
 }
+/*总结：1.使用虚拟头节点无需处理头节点的选择，不用手动判断空链表和单节点链表，而且出错概率低
+		2.刚开始本来想让list1为头然后依次和list2比较然后把list2插入list1，但是太复杂了，问ai优化为比较完直接连接在虚拟头节点后面
+*/
